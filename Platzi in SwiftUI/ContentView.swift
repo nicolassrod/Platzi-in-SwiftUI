3e904cf2fabd049b6320ff79e1bf7cdcc5dc77e4
Nicolás A. Rodríguez <nrq5510@gmail.com>
2020-06-05 17:57:27 -0500

    Initial Commit

---
+++ Platzi in SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Platzi in SwiftUI
+//
+//  Created by Nicolás A. Rodríguez on 6/5/20.
+//  Copyright © 2020 Nicolás A. Rodríguez. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
